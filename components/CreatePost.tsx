3ae66bca6e43a48fc422320b9c0b5f90cb5caeba
Manish674 <manishchoudhary4905@gmail.com>
2022-01-07 18:31:45 +0530

    adding theming

---
+++ components/CreatePost.tsx
@@ -0,0 +1,8 @@
+const CreatePost = () => {
+  return (
+    <div>
+      Hello world
+    </div>
+  )
+}
+export default CreatePost;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { DataContext } from "../utils/themeContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const defaultValue = {
+    user: {
+      username: '',
+      gmail: ''
+    },
+    theme: 'dark'
+  }
+
+  return (
+    <DataContext.Provider value={defaultValue}>
+      <Component {...pageProps} />
+    </DataContext.Provider>
+  )
 }
 
 export default MyApp
