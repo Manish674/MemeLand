6cb1d0f9bcb6f6949a821e6f1b83a99666dcde6b
Manish674 <manishchoudhary4905@gmail.com>
2022-01-19 16:41:07 +0530

    added context system to handle all the post data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { PostContextProvider } from '../utils/postContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PostContextProvider>
+      <Component {...pageProps} />{' '}
+    </PostContextProvider>
+  );
 }
 
 export default MyApp;
---
+++ utils/postContext.tsx
@@ -0,0 +1,35 @@
+import axios from './axios';
+import { createContext, useState } from 'react';
+
+const PostContext = createContext(null);
+
+const PostContextProvider = ({ children }: any) => {
+  const [hidden, setHidden] = useState(false);
+
+  const createPost = async (postData) => {
+    const { data } = await axios({
+      method: 'POST',
+      url: '/posts/',
+      headers: {
+        authorization: document.cookie,
+      },
+      data: {
+        postData,
+      },
+    });
+  };
+
+  return (
+    <PostContext.Provider
+      value={{
+        hidden,
+        setHidden,
+        createPost,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+};
+
+export { PostContext, PostContextProvider };
