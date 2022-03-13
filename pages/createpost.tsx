f21efec6b6c9bae3367e390847abbb94f1114953
Manish674 <manishchoudhary4905@gmail.com>
2022-03-13 09:26:07 +0530

    added navbar in createpost page

--- pages/createpost.tsx
+++ pages/createpost.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+
+import styles from '../styles/createpost.module.css';
+
 import CreatePost from '../components/CreatePost';
+import Sidebar from '../components/Sidebar';
 
 const createpost = () => {
-  return <CreatePost />;
+  return (
+    <div className={styles.mainContainer}>
+      <Sidebar />
+      <div className={styles.createPostWrapper}>
+        <CreatePost />
+      </div>
+    </div>
+  );
 };
 
 export default createpost;
