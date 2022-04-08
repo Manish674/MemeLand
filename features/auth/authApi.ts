9c1554c2780bff3968e168fa0bb71d8e663cdafa
Manish674 <manishchoudhary4905@gmail.com>
2022-04-08 07:39:51 +0530

    added register and login routes

---
+++ features/auth/authApi.ts
@@ -0,0 +1,58 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+interface Login {
+  email: string;
+  password: string;
+  token: string;
+}
+
+interface Register extends Login {
+  username: string;
+  dateOfBirth: string;
+}
+
+export const authApi = createApi({
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl:
+      process.env.NEXT_PUBLIC_ENV === 'dev'
+        ? 'http://localhost:8000/api/v1/auth/'
+        : `${process.env.NEXT_PUBLIC_URI}/api/v1/auth/`,
+  }),
+  endpoints: (builder) => ({
+    login: builder.mutation<void, Login>({
+      // Data is the args given to the useMutationLogin hook
+      // in this case data is { email, password, token }
+      query(data) {
+        return {
+          method: 'POST',
+          url: '/login',
+          headers: {
+            authentication: `Bearer ${data.token}`,
+          },
+          body: {
+            email: data.email,
+            password: data.password,
+          },
+        };
+      },
+    }),
+
+    register: builder.mutation<any, Omit<Register, 'token'>>({
+      query(data) {
+        return {
+          method: 'POST',
+          url: '/register',
+          body: {
+            username: data.username,
+            email: data.email,
+            password: data.password,
+            dateOfBirth: data.dateOfBirth,
+          },
+        };
+      },
+    }),
+  }),
+});
+
+export const { useLoginMutation, useRegisterMutation } = authApi;
--- utils/store.ts
+++ utils/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
-import { postApi } from './features/posts/postSlice';
+import { postApi } from '../features/posts/postApi';
+import { authApi } from '../features/auth/authApi';
 
 const store = configureStore({
   reducer: {
     [postApi.reducerPath]: postApi.reducer,
+    [authApi.reducerPath]: authApi.reducer,
   },
   // to enable caching and other useful features
   middleware: (getDefaultMiddleware) =>
